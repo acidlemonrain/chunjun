0f81926a55cc40c43a883151a51f93f8f03e880c
lemon <992273994@qq.com>
2022-08-15 16:13:22 +0800

    2

--- _next/static/fsy18F4nWwgAsI6OV_-iw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/955229c28454e75a.css","static/chunks/pages/index-75d800de036f747a.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
